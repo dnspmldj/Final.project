033f7a0a2a664b9afaa3bcc67eca0db665f160fb
dnspmldj <153061640+dnspmldj@users.noreply.github.com>
2023-12-07 23:40:30 +0800

    Update group3_final_project.py

--- group3_final_project.py
+++ group3_final_project.py
@@ -3,22 +3,6 @@ import time
 import sys
 import pandas as pd
 from PIL import Image
-from numpy import mean
-from numpy import std
-from numpy import argmax
-from sklearn.model_selection import KFold
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Dropout
-from keras.layers import BatchNormalization
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.models import load_model
-from keras.utils import load_img
-from keras.utils import img_to_array
-from keras.utils import to_categorical
 
 
 @st.cache(allow_output_mutation=True)
