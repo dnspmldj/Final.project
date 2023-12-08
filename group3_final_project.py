ea6864997c6835ce4f54dc88c17151bd597c4115
dnspmldj <153061640+dnspmldj@users.noreply.github.com>
2023-12-08 15:36:46 +0800

    Update group3_final_project.py

--- group3_final_project.py
+++ group3_final_project.py
@@ -1,83 +1,46 @@
 import streamlit as st
 import tensorflow as tf
-from keras.models import load_model
-from keras.utils import load_img
-from keras.utils import img_to_array
-from keras.utils import to_categorical
-from keras.models import Sequential
 import numpy as np
-from PIL import Image
-from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.preprocessing.image import img_to_array
-from keras.preprocessing.image import ImageDataGenerator
-from PIL import Image,ImageOps
-import numpy as np
-
-@st.cache_resource
-def load_model():
-  model=tf.keras.models.load_model('saved_fashion.h5')
-  return model
-model = load_model()
-st.write("""# Fashion Dataset by Group 3""")
-file=st.file_uploader("Choose photo from computer",type=["jpg","png"])
-
-# make a prediction for a new image.
-from tensorflow.keras.preprocessing.image import load_img
+from PIL import Image, ImageOps
 from tensorflow.keras.preprocessing.image import img_to_array
 from keras.models import load_model
 
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+@st.cache(allow_output_mutation=True)
+def load_fashion_model():
+    model = tf.keras.models.load_model('saved_fashion.h5')
+    return model
+
+def import_and_predict(image_data, model):
+    size = (64, 64)
+    image = ImageOps.fit(image_data, size)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
     return prediction
+
+def load_image(filename):
+    img = Image.open(filename).resize((64, 64))
+    img = img_to_array(img)
+    img = img / 255.0
+    img = np.reshape(img, (1, 64, 64, 3))
+    return img
+
+model = load_fashion_model()
+
+st.write("""# Fashion Dataset by Group 3""")
+file = st.file_uploader("Choose photo from computer", type=["jpg", "png"])
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['Tshirt','Top)','Pullover','Dress','Coat',
-                 'Sandal','Shirt','Sneaker','Bag','Ankle Boot']
-# load and prepare the image
-def load_image(filename):
-  img = Image.open(filename).resize((224, 224))
-  plt.imshow(img)
-  plt.show()
-  img = load_img(filename, target_size=(28, 28))
-  img = img_to_array(img)
-  img = img[:,:,0]
-  img = img.reshape(1,28, 28, 1)
-  img = img.astype('float32')
-  img = img / 255.0
-  return img
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    image_array = load_image(file)
+    prediction = import_and_predict(image_array, model)
+    
+    class_names = ['T-shirt', 'Top', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
 
-def run_example(filename):
-  img = load_image(filename)
-  model = load_model('/content/drive/MyDrive/Models/saved_fashion.h5')
-  result = np.argmax(model.predict(img), axis=1)
-  if result == 0:
-    print('Tshirt')
-  elif result == 1:
-    print('Top')
-  elif result == 2:
-    print('Pullover')
-  elif result == 3:
-    print('Dress')
-  elif result == 4:
-    print('Coat')
-  elif result == 5:
-    print('Sandal')
-  elif result == 6:
-    print('Shirt')
-  elif result == 7:
-    print('Snicker')
-  elif result == 8:
-    print('Bag')
-  else:
-    print('Ankle Boot')
-
-    string="OUTPUT : "+result[np.argmax(prediction)]
+    result_class = np.argmax(prediction)
+    result_label = class_names[result_class]
+    string = f"Prediction: {result_label} ({prediction[0][result_class]:.2%} confidence)"
     st.success(string)
