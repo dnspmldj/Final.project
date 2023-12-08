fbaac8feb2e9104b38d9b0afefd7f5263a875baf
dnspmldj <153061640+dnspmldj@users.noreply.github.com>
2023-12-08 15:34:49 +0800

    Update group3_final_project.py

--- group3_final_project.py
+++ group3_final_project.py
@@ -26,6 +26,21 @@ from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 from keras.models import load_model
 
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data,size)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['Tshirt','Top)','Pullover','Dress','Coat',
+                 'Sandal','Shirt','Sneaker','Bag','Ankle Boot']
 # load and prepare the image
 def load_image(filename):
   img = Image.open(filename).resize((224, 224))
