70fdc2113855957d36291a009ebe580422def21e
dnspmldj <153061640+dnspmldj@users.noreply.github.com>
2023-12-09 04:08:05 +0800

    Update copy_of_group3_final_project.py

--- copy_of_group3_final_project.py
+++ copy_of_group3_final_project.py
@@ -1,37 +1,60 @@
 import streamlit as st
-from PIL import Image, ImageOps
-import numpy as np
 import tensorflow as tf
+import numpy as np
+from PIL import Image, ImageOps
+from tensorflow.keras.preprocessing.image import img_to_array
+from keras.models import load_model
 
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def predict(image):
-    # Load the model inside the function to avoid caching issues
+@st.cache(allow_output_mutation=True)
+def load_fashion_model():
     model = tf.keras.models.load_model('saved_fashion.h5')
+    return model
 
+def import_and_predict(image_data, model):
     size = (64, 64)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    # Ensure image_data is in the correct data type and range
+    image_data = (image_data * 255).astype(np.uint8)
+
+    # Convert the NumPy array to an Image instance
+    image = Image.fromarray(image_data)
+
+    # Use ImageOps.fit with the Image instance
+    image = ImageOps.fit(image, size)
+
     img = np.asarray(image)
     img_reshape = img[np.newaxis, ...]
     prediction = model.predict(img_reshape)
     return prediction
 
-def main():
-    st.write("""# Fashion Dataset by Group 3""")
-    file = st.file_uploader("Choose photo from computer", type=["jpg", "png"])
+def load_image(filename):
+    img = Image.open(filename).resize((64, 64))
+    img = img_to_array(img)
+    
+    # Check if the image is grayscale, if so, add a channel dimension
+    if len(img.shape) == 2:
+        img = np.expand_dims(img, axis=-1)
+    
+    img = img / 255.0
+    img = np.reshape(img, (1, 64, 64, img.shape[-1]))
+    return img
 
-    if file is None:
-        st.text("Please upload an image file")
-    else:
-        image = Image.open(file)
-        st.image(image, use_column_width=True)
+model = load_fashion_model()
 
-        # Predictions are not cached to prevent UserHashError
-        prediction = predict(image)
+st.write("""# Fashion Dataset by Group 3""")
+file = st.file_uploader("Choose photo from computer", type=["jpg", "png"])
 
-        class_names = ['Tshirt', 'Top', 'Pullover', 'Dress', 'Coat',
-                       'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
-        result_string = "OUTPUT: " + class_names[np.argmax(prediction)]
-        st.success(result_string)
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    image_array = load_image(file)
+    prediction = import_and_predict(image_array, model)
+    
+    class_names = ['T-shirt', 'Top', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
 
-if __name__ == "__main__":
-    main()
+    result_class = np.argmax(prediction)
+    result_label = class_names[result_class]
+    string = f"Prediction: {result_label} ({prediction[0][result_class]:.2%} confidence)"
+    st.success(string)
