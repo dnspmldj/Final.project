7d581b6027ecd6d8789b25aeb2d8e5175929ed70
dnspmldj <153061640+dnspmldj@users.noreply.github.com>
2023-12-08 14:59:40 +0800

    Update group3_final_project.py

--- group3_final_project.py
+++ group3_final_project.py
@@ -1,37 +1,31 @@
 import streamlit as st
-from PIL import Image, ImageOps
-import numpy as np
 import tensorflow as tf
 
-# Load the model outside the function to improve efficiency
-model = tf.keras.models.load_model('saved_fashion.h5')
+from PIL import Image,ImageOps
+import numpy as np
 
-@st.cache(allow_output_mutation=True)
-def predict(image, model):
-    size = (64, 64)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
-    img = np.asarray(image)
-    img_reshape = img[np.newaxis, ...]
-    prediction = model.predict(img_reshape)
+@st.cache_resource
+def load_model():
+  model=tf.keras.models.load_model('saved_fashion.h5')
+  return model
+model = load_model()
+st.write("""# Fashion Dataset by Group 3""")
+file=st.file_uploader("Choose photo from computer",type=["jpg","png"])
+
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
     return prediction
-
-def main():
-    st.write("""# Fashion Dataset by Group 3""")
-    file = st.file_uploader("Choose photo from computer", type=["jpg", "png"])
-
-    if file is None:
-        st.text("Please upload an image file")
-    else:
-        image = Image.open(file)
-        st.image(image, use_column_width=True)
-
-        # Predictions are cached to improve responsiveness
-        prediction = predict(image, model)
-
-        class_names = ['Tshirt', 'Top', 'Pullover', 'Dress', 'Coat',
-                       'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
-        result_string = "OUTPUT: " + class_names[np.argmax(prediction)]
-        st.success(result_string)
-
-if __name__ == "__main__":
-    main()
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['Tshirt','Top)','Pullover','Dress','Coat',
+                 'Sandal','Shirt','Sneaker','Bag','Ankle Boot']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
