0d24b9608f1a1bb60f4a669170fffaccdcd7241d
dnspmldj <153061640+dnspmldj@users.noreply.github.com>
2023-12-08 10:53:47 +0800

    Update group3_final_project.py

--- group3_final_project.py
+++ group3_final_project.py
@@ -4,12 +4,17 @@ import sys
 import pandas as pd
 from PIL import Image
 
+@st.cache_resource
 
-@st.cache(allow_output_mutation=True)
 def load_model():
   model=tf.keras.models.load_model('saved_fashion.h5')
   return model
-  
+
+model = load_model()
+st.write("""# Fashion Dataset by Group 3""")
+file=st.file_uploader("Choose clothes photo from computer",type=["jpg","png"])
+
+import cv2
 from PIL import Image,ImageOps
 import numpy as np
 def import_and_predict(image_data,model):
@@ -19,49 +24,14 @@ def import_and_predict(image_data,model):
     img_reshape=img[np.newaxis,...]
     prediction=model.predict(img_reshape)
     return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['Tshirt','Top)','Pullover','Dress','Coat',
+                 'Sandal','Shirt','Sneaker','Bag','Ankle Boot']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
 
-st.write("""# Fasion""")
-
-file=st.file_uploader("Choose clothes photo from computer",type=["jpg","png"])
-
-def load_image(filename):
-  img = Image.open(filename).resize((224, 224))
-  plt.imshow(img)
-  plt.show()
-  img = load_img(filename, target_size=(28, 28))
-  img = img_to_array(img)
-  img = img[:,:,0]
-  img = img.reshape(1,28, 28, 1)
-  img = img.astype('float32')
-  img = img / 255.0
-  return img
-
-def run_example(filename):
-  img = load_image(filename)
-  model=tf.keras.models.load_model('saved_fashion.h5')
-  result = np.argmax(model.predict(img), axis=1)
-  if result == 0:
-    print('Tshirt')
-  elif result == 1:
-    print('Top')
-  elif result == 2:
-    print('Pullover')
-  elif result == 3:
-    print('Dress')
-  elif result == 4:
-    print('Coat')
-  elif result == 5:
-    print('Sandal')
-  elif result == 6:
-    print('Shirt')
-  elif result == 7:
-    print('Snicker')
-  elif result == 8:
-    print('Bag')
-  else:
-    print('Ankle Boot')
-    
-    
-string="OUTPUT : "+result[np.argmax(prediction)]
-
-st.success(string)
